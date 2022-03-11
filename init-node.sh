ac5886b9a21450e98da9c8049c442604f9dd52ad
Mridul Singh <77195905+zypher9@users.noreply.github.com>
2022-03-11 22:16:54 +0530

    Create init-node.sh
    initialising node on Linux based systems

---
+++ init-node.sh
@@ -0,0 +1,7 @@
+cd ~
+wget https://dist.ipfs.io/ipfs-update/v1.8.0/ipfs-update_v1.8.0_linux-amd64.tar.gz
+tar xvfz ipfs-update_v1.8.0_linux-amd64.tar.gz
+cd ipfs-update
+sudo ./install.sh
+ipfs-update versions
+ipfs-update install latest
